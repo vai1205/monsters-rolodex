bcb5a484d84b55b1339463b63bfd7cb0351abe22
vai1205 <vaibhav.12may95@gmail.com>
2019-10-13 20:52:39 +0530

    Updates

---
+++ precache-manifest.b5bafd2b9c899f0bd2829a304dfea2fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c045ea6c28f4bf72c5c5c5b848bba76b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8c715a088d9520973919",
+    "url": "/monsters-rolodex/static/css/main.ede8fd4b.chunk.css"
+  },
+  {
+    "revision": "e24bf259d86190d46326",
+    "url": "/monsters-rolodex/static/js/2.48c9588f.chunk.js"
+  },
+  {
+    "revision": "8c715a088d9520973919",
+    "url": "/monsters-rolodex/static/js/main.0eb27a98.chunk.js"
+  },
+  {
+    "revision": "73ec52f4272055bea411",
+    "url": "/monsters-rolodex/static/js/runtime-main.1cffd587.js"
+  }
+]);
